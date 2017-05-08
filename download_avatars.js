c29d22177f6dcd5b9cc4be8a5d5e58d79e6f7b23
ffiargus <rm.chow@outlook.com>
2017-05-08 23:28:53 +0000

    Added error handling

--- download_avatars.js
+++ download_avatars.js
@@ -5,16 +5,14 @@ require('dotenv').config();
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var URLs = {};
-var GITHUB_USER = "ffiargus";
+var GITHUB_USER = process.env.user;
 var GITHUB_TOKEN = process.env.token;
 var USER_AGENT = "GitHub Avatar Downloader - Student Project";
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-    // console.log("Status Code: " + response.statusCode);
-    // console.log("Body: " + response);
-    // console.log(response);
+
   var options = {
     "url": requestURL,
     "headers": {
@@ -24,33 +22,37 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request.get(options, function (err, response) {
     cb(err, JSON.parse(response.body));
   });
-
-  // request(options, function(err, response, body) {
-  //   cb(err, JSON.parse(response.body));
 }
-  // })
-  //cb(response.statusCode, response);
-  //console.log(requestURL);
+
+
+
 function downloadImageByURL(url, filePath) {
-//function downloadImageByURL("https:\/\/avatars2.githubusercontent.com\/u\/2741?v=3&s=466", "avatars/kvirani.jpg") {
-  request.get(url)
-       .pipe(fs.createWriteStream(filePath));
+  if (fs.existsSync(filePath)) {
+    request.get(url)
+         .pipe(fs.createWriteStream(filePath));
+  }
+  else {
+    throw "The file directory [" + filePath + "] does not exist.";
+  }
 }
 
 var owner = process.argv[2];
 var repo = process.argv[3];
+if (!fs.existsSync("./.env"))
+  throw ".env is missing!";
+if (!process.env.token)
+  throw "GitHub token not found";
+if (process.argv[4])
+  throw "Invalid arguements, please enter the owner followed by the repo.";
 
 if (owner != null && repo != null){
   getRepoContributors(owner, repo, function(err, result) {
+    if (!result[0])
+      throw "The repo does not exist, or invalid credentials";
     console.log("Errors:", err);
-    //console.log("Result:", result);
     for (person of result){
-      //console.log(person.avatar_url);
-      URLs[person.login] = person.avatar_url;
-      console.log(URLs[person.login]);
       downloadImageByURL(person.avatar_url, 'avatars/'+person.login+'.jpg')
     }
-
   });
 } else {
   console.log("Invalid arguements, please enter the owner followed by the repo.")
