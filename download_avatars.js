9a940ec24a493733f73ea3802ec0250d3641cc0b
ffiargus <rm.chow@outlook.com>
2017-05-08 20:41:36 +0000

    complets step 4

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "ffiargus";
+var GITHUB_TOKEN = "";
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
